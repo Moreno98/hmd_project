e76ab5e1e327cda5e03243d5f329002bc243f7d0
Moreno98 <morenodinca@gmail.com>
2022-02-01 11:47:31 +0100

    Experiments with custom actions

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,23 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import random
+from rasa_sdk.events import SlotSet
+
+
+class Query_db(Action):
+
+    def name(self) -> Text:
+        return "query_db"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        product = tracker.get_slot("product_to_buy")
+        bool = random.randint(0, 1)
+        return [SlotSet("product_query", True if bool == 1 else False)]
